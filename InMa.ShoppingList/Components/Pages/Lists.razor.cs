39e95ab874c3898f4089cd587ec42d86932b1de3
pvyron <pvyron@gmail.com>
2024-03-26 00:02:00 +0200

    create project from fluent ui template and add bare bones shopping list page

---
+++ InMa.ShoppingList/Components/Pages/Lists.razor.cs
@@ -0,0 +1,44 @@
+﻿using InMa.ShoppingList.Models;
+
+namespace InMa.ShoppingList.Components.Pages;
+
+public partial class Lists
+{
+    private IList<ShoppingListItem> Items { get; set; }
+    
+    private string NewProductName { get; set; }
+    private bool AddingProduct { get; set; } = false;
+
+    public Lists()
+    {
+        Items = new List<ShoppingListItem>()
+        {
+            new ShoppingListItem("Bread"),
+            new ShoppingListItem("CocaCola")
+        };
+    }
+
+    async Task AddNewProduct()
+    {
+        try
+        {
+            AddingProduct = true;
+            
+            if (string.IsNullOrWhiteSpace(NewProductName))
+            {
+                return;
+            }
+
+            if (Items.Any(i => i.Product.Equals(NewProductName, StringComparison.OrdinalIgnoreCase)))
+            {
+                return;
+            }
+
+            Items.Add(new ShoppingListItem(NewProductName));
+        }
+        finally
+        {
+            AddingProduct = false;
+        }
+    }
+}
---
+++ InMa.ShoppingList/Models/ShoppingListItem.cs
@@ -0,0 +1,7 @@
+﻿namespace InMa.ShoppingList.Models;
+
+public sealed class ShoppingListItem(string product)
+{
+    public bool? Bought { get; set; }
+    public string Product { get; set; } = product;
+}
