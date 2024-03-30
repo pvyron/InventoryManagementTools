39a0097ec6535ad18cc477a896220235e4447ae6
pvyron <pvyron@gmail.com>
2024-03-30 15:08:20 +0200

    transition to postgres because of Neon free tier

--- InMa.Shopping/Program.cs
+++ InMa.Shopping/Program.cs
@@ -28,10 +28,10 @@ builder.Services.AddAuthentication(options =>
     })
     .AddIdentityCookies();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
-                       throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+var connectionString = builder.Configuration.GetConnectionString("AuthDb") ??
+                       throw new InvalidOperationException("Connection string 'AuthDb' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
