7d0864655448664896dfd73f13e8b7c800491bc1
pvyron <pvyron@gmail.com>
2024-06-11 17:47:35 +0300

    testing flexible auth

---
+++ InMa.Shopping/Components/Account/IdentityClaimsProvider.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+using InMa.Shopping.DomainEnums;
+
+namespace InMa.Shopping.Components.Account;
+
+public sealed class IdentityClaimsProvider
+{
+    private const string AUTH_CLAIM_TYPE_NAME = "pvyronAuth";
+    
+    public static IEnumerable<Claim> InitializeCustomClaims()
+    {
+        var authorizationClaim = new Claim(AUTH_CLAIM_TYPE_NAME, ((byte)PvyronAuthorizations.ShoppingListsComplete).ToString());
+        var authorizationClaim3 = new Claim(AUTH_CLAIM_TYPE_NAME, ((byte)PvyronAuthorizations.ShoppingLists).ToString());
+
+        return [authorizationClaim, authorizationClaim3];
+    }
+    
+    
+}
---
+++ InMa.Shopping/DomainEnums/PvyronAuthorizations.cs
@@ -0,0 +1,13 @@
+﻿namespace InMa.Shopping.DomainEnums;
+
+public enum PvyronAuthorizations : byte
+{
+    ShoppingLists = 1, //10000000
+    ShoppingListsComplete = 2, //01000000
+    FilesUpload = 4, //00100000
+    PlaceHolder1 = 8, //00010000
+    PlaceHolder2 = 16, //00001000
+    PlaceHolder3 = 32, //00000100
+    PlaceHolder4 = 64, //00000010
+    PlaceHolder5 = 128, //00000001
+}
